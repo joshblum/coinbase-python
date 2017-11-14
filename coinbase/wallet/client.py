7c8d92ff8f1a318c9e1e5c5d567b40c8cc91618d
Joshua Blum <jblum18@gmail.com>
2017-11-14 15:27:47 -0500

    Update client.py (#7)

--- coinbase/wallet/client.py
+++ coinbase/wallet/client.py
@@ -65,7 +65,7 @@ class Client(object):
   VERIFY_SSL = True
 
   BASE_API_URI = 'https://api.coinbase.com/'
-  API_VERSION = '2017-08-09'
+  API_VERSION = '2017-05-19'
 
   cached_callback_public_key = None
 
